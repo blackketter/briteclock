37f6aba7ad41b0505b4222fd3931eb61cd614f48
Dean Blackketter <dean@blackketter.com>
2017-01-09 07:43:48 -0800

    Use updated Clock and WiFiThing libraries. Improve basic functionality.

--- src/briteclock.ino
+++ src/briteclock.ino
@@ -4,38 +4,45 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
-#include "TimeLib.h"
-#include "myWifi.h"
-#include "WiFiConsole.h"
+#include "WiFiThing.h"
 #include "Clock.h"
+// create Credentials.h and define const char* ssid and passphrase
+#include "Credentials.h"
 
-WiFiConsole console;
+// platformio seems to need these FIXME
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <NTPClient.h>
+#include <ArduinoOTA.h>
 
 #define TFT_DC     2
 #define TFT_CS     4
 #define TFT_BL     0
+#define TFT_RST    16
 #define LIGHT_SENSOR (A0)
 
+#define ANALOGRANGE (1024)
 #define BUTTON_PIN (D1)
+
 Switch button = Switch(BUTTON_PIN);  // Switch between a digital pin and GND
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
-Clock clock = Clock();
+Clock clock;
+Clock paris;
 
-// todo - make this configurable
+WiFiThing thing;
+// WiFiConsole console is provided by WiFiThing
 
 void setup() {
 
-  setupWifi("briteclock", -7*60*60);
-
-  console.begin();
+  thing.begin(ssid, passphrase);
 
   pinMode(TFT_BL, OUTPUT);
-  analogWrite(TFT_BL, 1);
+  analogWrite(TFT_BL, 0);
 
   tft.begin();
-  tft.setRotation(1);
+  tft.setRotation(3);
 
   // read diagnostics (optional but can help debug problems)
   uint8_t x = tft.readcommand8(ILI9341_RDMODE);
@@ -50,23 +57,33 @@ void setup() {
   console.print("Self Diagnostic: 0x"); console.println(x, HEX);
 
   tft.fillScreen(ILI9341_BLACK);
+
+  paris.setZoneOffset(1*60*60);
+  clock.setZoneOffset(-8*60*60);
 }
 
 
 void loop(void) {
-  console.loop();
 
-//  console.debugln("polling");
   button.poll();
 
-//  console.debugln("wifi");
-  loopWifi();
+  thing.loop();
 
   static time_t lastTime = 0;
   static int fps = 0;
   static int lastfps = 0;
-  if (now() != lastTime) {
-    lastTime = now();
+
+  bool redraw = false;
+
+  // redraw every hour
+  static int lastHour = 0;
+  if (lastHour != clock.hour()) {
+    lastHour = clock.hour();
+    redraw = true;
+  }
+
+  if (Uptime::seconds() != lastTime) {
+    lastTime = Uptime::seconds();
     char time[100];
     clock.longTime(time);
     console.debugln(time);
@@ -79,56 +96,90 @@ void loop(void) {
     fps++;
   }
 
-  static uint32_t b = 0;
-
-  if (button.pushed()) {
+  static bool screenoff = false;
+  if (button.released() || button.longPress()) {
+    redraw = true;
   }
 
-  int h = hour();
-  int m = minute();
-  if (h > 12) { h -= 12; }
-  if (h == 0) { h = 12; }
+  if (button.pushed()) {
+    screenoff = !screenoff;
+  }
+
+
+  static uint32_t b = 0;
+  uint32_t ambient = analogRead(LIGHT_SENSOR);
+  b = (ambient*PWMRANGE)/ANALOGRANGE;
+  if (b == 0) { b = 1; }
+  if (screenoff && !button.on()) {
+    b = 0;
+  }
+
+  analogWrite(TFT_BL, b);
+
+  if (redraw) {
+    tft.fillScreen(ILI9341_BLACK);
+  }
 
   tft.setCursor(0, 0);
-  tft.setTextSize(9);
+  tft.setTextSize(2);
   uint16_t c = ILI9341_RED;
   tft.setTextColor(c, ILI9341_BLACK);
-  tft.printf("\n%2d:%02d",h,m);
 
-  tft.setTextSize(2);
-  tft.printf("%02d",second());
+  if (button.longPressLatch()) {
+    // draw info
+    tft.printf("Time: %2d:%02d:%02d.%01d\n",clock.hourFormat12(),clock.minute(),clock.second(),clock.fracMillis()/100);
+    tft.printf("wifi: %s\n", WiFi.isConnected() ? "connected   " : "disconnected");
+    tft.printf("uptime: %d\n", Uptime::seconds());
 
-  tft.setTextSize(9);
-  tft.println();
+    String hostname = thing.getHostname();
+    tft.printf("host:%s.local\n", hostname.c_str());
+    String ip = thing.getIPAddress();
+    tft.printf("ip: %s\n", ip.c_str());;
 
-  tft.setTextSize(2);
+    tft.printf("ambient: %4d\n", ambient);
+    tft.printf("brightness: %4d\n", b);
 
-  tft.printf("wifi: %d\n", WiFi.isConnected());
-  uint32_t ambient = analogRead(LIGHT_SENSOR);
-  tft.printf("ambient: %4d\n", ambient);
-  b = PWMRANGE - ambient;
-  analogWrite(TFT_BL, b);
-  tft.printf("brightness: %4d\n", b);
-
-     static int16_t x = 0;
+    static int16_t x = 0;
     static uint32_t lastmillis = millis();
     uint32_t ms = millis() - lastmillis;
     lastmillis = millis();
 
     tft.printf("fps: %2d (%4d)ms\n", lastfps, ms);
-//  console.debugln("done");
 
-    const int maxheight = 30;
-    const int maxmillis = 500;
-    uint16_t height = (ms * maxheight)/ maxmillis;
-    if (height > maxheight) { height = maxheight; }
-    if (ms > 49) { ms = 49; }
-    tft.drawLine(x,239-height,x,239-maxheight,ILI9341_BLACK);
-    tft.drawLine(x,239,x,239-height, ILI9341_BLUE);
-    x++;
-    if (x>319) { x = 0; }
+  } else {
+    // draw clock
+    if (clock.hasBeenSet()) {
+      tft.printf("Paris: %d:%02d%s, %s", paris.hourFormat12(), paris.minute(), paris.isAM() ? "am":"pm", paris.weekdayString());
+    } else if (!WiFi.isConnected()){
+      tft.println("Connecting...");
+    } else {
+      tft.println("Trying to set time...");
+    }
 
+    tft.setTextSize(10);
+    tft.println();
+    if (clock.hasBeenSet()) {
 
+      tft.printf("%2d:%02d",clock.hourFormat12(),clock.minute());
+      tft.setTextSize(1);
+      tft.printf("%02d",clock.second());
+    }
+
+    tft.setTextSize(10);
+    tft.println();
+
+    tft.setTextSize(2);
+
+    if (clock.hasBeenSet()) {
+      char longdate[100];
+      sprintf(longdate, "%s, %s %d", clock.weekdayString(), clock.monthString(), clock.day());
+      int spaces = (320/(2*6) - strlen(longdate))/2;
+      for (int i = 0; i < spaces; i++) {
+        tft.print(" ");
+      }
+      tft.print(longdate);
+    }
+  }
 }
 
 
